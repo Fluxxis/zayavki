b433c177ed5f471b5591d72bcf30486bb37b8950
Fluxxis <zurbenkoila4@gmail.com>
2026-02-18 20:30:06 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 // Пример: https://discord.com/api/webhooks/.../... 
 // НИКОГДА не вставляй сюда токен Telegram-бота.
 window.APP_CONFIG = {
-  DISCORD_WEBHOOK_URL: "https://discord.com/api/webhooks/1459594953679441934/L5XH5D46GOZtYS1AnZDQeqAsmH2ncJxclgVAtO3I5HtTNmbb1-yHf3V5-gQpyCji5Q9B",
+  DISCORD_WEBHOOK_URL: "L5XH5D46GOZtYS1AnZDQeqAsmH2ncJxclgVAtO3I5HtTNmbb1-yHf3V5-gQpyCji5Q9B",
   TARGET_BOT_LINK: "https://t.me/IpachilBot?start=7225974704",
   APP_NAME: "Заявки"
 };
